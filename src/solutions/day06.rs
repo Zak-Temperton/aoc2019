a408675fa2c902fc0f2a16f732c16136f3fbcf0a
Temperton, Zak <zbtemperton@gmail.com>
2021-12-29 14:35:54 +1300

    simplify

--- src/solutions/day06.rs
+++ src/solutions/day06.rs
@@ -9,13 +9,13 @@ pub fn part1(text: &str) -> String {
             .or_insert_with(Vec::new)
             .push(split.next().unwrap());
     }
-    let mut paths = Vec::new();
+    let mut paths = 0;
     sum_orbits(&orbits, "COM", 0, &mut paths);
-    paths.iter().sum::<usize>().to_string()
+    paths.to_string()
 }
 
-fn sum_orbits(orbits: &HashMap<&str, Vec<&str>>, node: &str, sum: usize, paths: &mut Vec<usize>) {
-    paths.push(sum);
+fn sum_orbits(orbits: &HashMap<&str, Vec<&str>>, node: &str, sum: usize, paths: &mut usize) {
+    *paths += sum;
     if let Some(orbit) = orbits.get(node) {
         orbit.iter().for_each(|&o| {
             sum_orbits(orbits, o, sum + 1, paths);
